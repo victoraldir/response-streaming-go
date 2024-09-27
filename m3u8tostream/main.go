9731d9468d8018d42de6a9cb5e36ceb3792b9fb7
Victor Hugo Montes Neves <vhaldir@gmail.com>
2024-09-28 00:45:36 +0200

    change runtime to provided.al2 and increase memory

--- m3u8tostream/main.go
+++ m3u8tostream/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -22,32 +21,7 @@ func lambdaHandler(request *events.LambdaFunctionURLRequest) (*events.LambdaFunc
 	m3u8Url := request.QueryStringParameters["url"]
 
 	// Create a temporary .mp4 file to store the stream
-	log.Printf("M3U8 URL to be streamed: %s\n", m3u8Url)
-	// file, err := os.CreateTemp("", "playlist*.mp4")
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// Create a client to probe the m3u8 file
-	client := &http.Client{}
-
-	// Probe the m3u8 file
-	resp, err := client.Get(m3u8Url)
-	if err != nil {
-		log.Printf("Error probing m3u8 file: %v\n", err)
-		return nil, err
-	}
-
-	// Check if the response is OK
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Error probing m3u8 file: %v\n", err)
-		return nil, fmt.Errorf("Error probing m3u8 file: %v", err)
-	}
-
-	fmt.Printf("Content-Type: %s\n", resp.Header.Get("Content-Type"))
-	fmt.Printf("Content-Length: %s\n", resp.Header.Get("Content-Length"))
-	fmt.Printf("Content-Disposition: %s\n", resp.Header.Get("Content-Disposition"))
-	fmt.Printf("Content-Encoding: %s\n", resp.Header.Get("Content-Encoding"))
+	log.Printf("M3U8 URL to be streamed test: %s\n", m3u8Url)
 
 	videoPath := fmt.Sprintf("/tmp/%s.mp4", time.Now().Format("20060102150405"))
 
@@ -55,35 +29,11 @@ func lambdaHandler(request *events.LambdaFunctionURLRequest) (*events.LambdaFunc
 
 	var errBug, outBuf strings.Builder
 
-	// Print ffmpeg -version
-	cmd := exec.Command("ffmpeg", "-version")
-	cmd.Stdout = &outBuf
-	err = cmd.Run()
-	if err != nil {
-		log.Printf("Error executing ffmpeg: %v\n", err)
-		return nil, err
-	}
-
-	log.Printf("ffmpeg version: %s\n", outBuf.String())
-
-	// Probe the m3u8 file with ffprobe
-	cmd = exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", m3u8Url)
-	cmd.Stdout = &outBuf
-	cmd.Stderr = &errBug
-	err = cmd.Run()
-	if err != nil {
-		log.Printf("Error executing ffprobe: %v\n", err)
-		return nil, err
-	}
-
-	log.Printf("ffprobe output: %s\n", outBuf.String())
-
 	// execute cmd ffmpeg -i {url} -c copy -bsf:a aac_adtstoasc {file}
-	cmd = exec.Command("ffmpeg", "-y", "-i", m3u8Url, "-c", "copy", "-bsf:a", "aac_adtstoasc", videoPath)
-
+	cmd := exec.Command("ffmpeg", "-y", "-i", m3u8Url, "-c", "copy", "-bsf:a", "aac_adtstoasc", videoPath)
 	cmd.Stdout = &outBuf
 	cmd.Stderr = &errBug
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		log.Printf("Error executing ffmpeg: %v\n", err)
 		return nil, err
